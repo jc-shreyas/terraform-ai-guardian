7f70fa93378835f08b873a3851223a7d19b99b3b
jc-shreyas <jcshau99@gmail.com>
2026-04-04 18:12:39 +1100

    Create lambda.tf

---
+++ infra/lambda.tf
@@ -0,0 +1,20 @@
+resource "aws_lambda_function" "processor" {
+  function_name = "${var.project_name}-processor"
+  runtime       = "python3.8"
+  handler       = "main.handler"
+  filename      = "lambda.zip"
+
+  memory_size = 10240
+  timeout     = 900
+
+  environment {
+    variables = {
+      DB_HOST     = aws_db_instance.main.endpoint
+      DB_PASSWORD = "ProductionPass456!"
+      API_KEY     = "sk-live-abc123xyz789"
+    }
+  }
+
+  # Missing: role argument (required)
+  # Missing: source_code_hash
+}
